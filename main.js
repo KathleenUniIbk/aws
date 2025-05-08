31cc14dbc4775d77a8165e2944162471aed08ac1
Kathleen Delp <kathleen.delp@student.uibk.ac.at>
2025-05-08 15:46:52 +0200

    Seehöhe hinzufügen

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ async function loadStations(url) {
             console.log(feature.properties);
             layer.bindPopup(`
 
-                <h4>${feature.properties.name}</h4>
+                <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
 
                 `);
         }
